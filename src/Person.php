b5d986c7ed7547009dd8839006cfbc75c071872b
wasaby <wasaby.stnc@gmail.com>
2019-10-08 20:28:09 +0300

    Initial commit

---
+++ src/Person.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PHP
+ * Date: 08.10.2019
+ * Time: 20:21
+ */
+
+class Person
+{
+    public $firstName;
+    private $lastName;
+    protected $age;
+}
+
+class  Admin extends Person
+{
+
+}
