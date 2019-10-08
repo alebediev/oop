ea2e56f07638835418dbebf40178b83ea8945f7b
wasaby <wasaby.stnc@gmail.com>
2019-10-08 20:42:33 +0300

    Del .idea from git

--- src/Person.php
+++ src/Person.php
@@ -8,9 +8,60 @@
 
 class Person
 {
-    public $firstName;
+    private $firstName;
     private $lastName;
-    protected $age;
+    private $age;
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+
+
 }
 
 class  Admin extends Person
